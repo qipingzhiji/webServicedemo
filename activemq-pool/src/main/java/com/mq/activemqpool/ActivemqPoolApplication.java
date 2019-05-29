b430ec7bbe0e596750680d4e34e184b33cd4e7bc
qipingzhiji <1213462884@qq.com>
2019-05-29 13:38:25 +0800

    activemq

---
+++ activemq-pool/src/main/java/com/mq/activemqpool/ActivemqPoolApplication.java
@@ -0,0 +1,15 @@
+package com.mq.activemqpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+@EnableJms
+public class ActivemqPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqPoolApplication.class, args);
+	}
+
+}
---
+++ activemq-pool/src/test/java/com/mq/activemqpool/ActivemqPoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mq.activemqpool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivemqPoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
